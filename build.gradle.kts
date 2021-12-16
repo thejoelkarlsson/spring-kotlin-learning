ca5e6ef884dc5b779e0deff9181cec6a7d8792e1
Joel Karlsson <thejoelkarlsson@gmail.com>
2021-12-16 20:44:52 +0100

    Add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,14 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
